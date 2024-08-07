2a0ece7e154cf54632929c5dec0fdabab018eecc
Tanzer ahammad <166015687+tanzerahammad@users.noreply.github.com>
2024-08-07 16:35:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*ex1*/
+function showText() {
+   document.getElementById('s_text').innerHTML="hellow ssb";
+}
+/*ex2*/
+function showText2() {
+   document.getElementById('s_text2').innerHTML="hellow ssb2";
+}
+/*ex3*/
+function showText3() {
+   document.getElementById('s_text3').innerHTML="hellow ssb3";
+}
+/*ex4*/
+function showText4() {
+   document.getElementById('s_text4').innerHTML="hellow ssb4";
+}
+/*ex5*/
+function showText5() {
+   document.getElementById('s_text5').innerHTML="hellow ssb5";
+}
+/*ex6*/
+function showText6() {
+   document.getElementById('s_text6').innerHTML="hellow ssb6";
+}
+/*ex7*/
+function showText7() {
+   document.getElementById('s_text7').innerHTML="hellow ssb7";
+}
+/*ex8*/
+function showText8() {
+   document.getElementById('s_text8').innerHTML="hellow ssb8";
+}
+/*ex9*/
+function showText9() {
+   document.getElementById('s_text9').innerHTML="hellow ssb9";
+}
+/*ex10*/
+function showText10() {
+   document.getElementById('s_text10').innerHTML="hellow ssb10";
+}
